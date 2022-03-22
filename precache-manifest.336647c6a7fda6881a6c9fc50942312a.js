b3df9dfd339673b83858735e0ca9514211538a6d
Ridhim Garg <ridhimgarg@gmail.com>
2022-03-22 18:56:26 +0530

    Updates

---
+++ precache-manifest.336647c6a7fda6881a6c9fc50942312a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b99b7a2d182e59110ad1",
+    "url": "/chatbotApp/static/css/main.a4618d4b.chunk.css"
+  },
+  {
+    "revision": "b99b7a2d182e59110ad1",
+    "url": "/chatbotApp/static/js/main.b99b7a2d.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/chatbotApp/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "26fb0e3f956d1da5f55c",
+    "url": "/chatbotApp/static/css/2.6ef0371f.chunk.css"
+  },
+  {
+    "revision": "26fb0e3f956d1da5f55c",
+    "url": "/chatbotApp/static/js/2.26fb0e3f.chunk.js"
+  },
+  {
+    "revision": "e577753a972f50d4d619",
+    "url": "/chatbotApp/static/js/runtime~main.e577753a.js"
+  },
+  {
+    "revision": "2f9160f1e0076e6d9be217222454e714",
+    "url": "/chatbotApp/index.html"
+  }
+];
